fae6067a5fcb372cf1b75e0bb55fc5fe3665614d
al <>
2024-02-12 16:16:34 -0800

    minor fix to name of function

--- complex_type_funcs.c
+++ complex_type_funcs.c
@@ -2,7 +2,7 @@
 
 #include "complex_type_funcs.h"
 
-struct char10_struct private_things_func(struct char10_struct value)
+struct char10_struct return_char10_struct(struct char10_struct value)
 {
     return value;
 }
